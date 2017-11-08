3342ee2d3cf4fe862b82592a862623400773f709
LucianoSilva27 <32067428+LucianoSilva27@users.noreply.github.com>
2017-11-08 20:42:57 -0200

    Update TREEORD.java

--- TREEORD.java
+++ TREEORD.java
@@ -3,42 +3,33 @@ import java.lang.*;
 
 
  class TREEORD {
-     //O objetivo deste exercicio é verificar:
-    // 1.Se o primeiro nó da primeira árvore é igual o utlimo nó da segunda árvore;
-    // 2.Se o ultimo nó da primeira árvore é igual o ultimo da ultima árvore;
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) throws java.lang.Exception
     {
-        // TODO code application logic here
+       
         Scanner entrada = new Scanner(System.in);
         int no,cont= 0;
         int qtd = entrada.nextInt();
         int[] aux = new int[2];
-        for(int i=0;i<3;i++)//Pega cada linha 
+        for(int i=0;i<3;i++)
         {
         for (int j=0;j<qtd;j++){
-            //Esse for vai verificando cada nó da arvore
+           
             no = entrada.nextInt();
             if(i==0 && j==0){ 
-                //Preenche o array com o primeiro elemento da arvore
-                aux[0] = no;
+               aux[0] = no;
             }
 
             if(i==0 && j==qtd-1) {
-                //primeira árvore,ultimo elemento
-                aux[1] = no;
+               aux[1] = no;
             }
 
             if((i==1 && j==qtd-1) && aux[0]==no) {
-                //Segunda árvore,ultimo elemento,verifica se o primeiro elemento do array aux é igual o nó que esta sendo informado.
-                cont++;
+               cont++;
             }
 
             if(i==2 && j==qtd-1 && aux[1]==no) {
-                //terceira árvore,verifica se um ultimo elemento de aux é igual o nó fornecido;
-                cont++;
+               cont++;
             }
         }
     }
